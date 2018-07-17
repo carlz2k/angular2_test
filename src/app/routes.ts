e1dbc121c8c559a422e5a44bfbfa3ffc0dd920ce
Carl Zhang <carl.zhang@payfirma.com>
2018-07-17 13:44:26 -0700

    first checkin

---
+++ src/app/routes.ts
@@ -0,0 +1,20 @@
+import {Injectable} from '@angular/core';
+import {UIRouter, Ng2StateDeclaration} from 'ui-router-ng2';
+
+import {AppComponent} from './containers/App';
+
+export const STATES: Ng2StateDeclaration[] = [
+  {
+    name: 'App',
+    url: '/',
+    component: AppComponent
+  }
+];
+
+@Injectable()
+export class MyUIRouterConfig {
+  configure(uiRouter: UIRouter) {
+    let a = 5;
+    uiRouter.urlRouterProvider.otherwise('App');
+  }
+}
